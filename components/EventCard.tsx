19d4614ce6b9d0ab4a41d3b20c726303af156c71
igorcol <igor.colombini@gmail.com>
2025-03-29 03:47:10 -0300

    feat: Event Presente Btns

--- components/EventCard.tsx
+++ components/EventCard.tsx
@@ -1,18 +1,35 @@
-import { Calendar, Clock, MapPin } from "lucide-react";
-import React from "react";
-import { Button } from "./ui/button";
+"use client";
+import { Calendar, Check, Clock, MapPin, X } from "lucide-react";
+import React, { useState } from "react";
+import { cn } from "@/lib/utils";
 
 const EventCard = () => {
+  const [isPresente, setPresente] = useState<boolean | null>(null); // TODO: Trocar para presença no DB
+
   return (
     <div className="bg-white w-full rounded-lg shadow-xs border border-border py-4 px-5 flex flex-col space-y-5">
       {/* Dados */}
       <div className="w-full space-y-2">
         {/* Titulo */}
-        <div>
-          <div className="badge badge-primary rounded-xl w-max px-3 font-semibold">
-            Reunião
+        <div className="flex flex-row justify-between">
+          <div>
+            <div className="badge badge-primary rounded-xl w-max px-3 font-semibold">
+              Reunião
+            </div>
+            <h1 className="font-bold text-xl">Reunião Ritualistica</h1>
           </div>
-          <h1 className="font-bold text-xl">Reunião Ritualistica</h1>
+
+          {/* Badge Presença */}
+          {isPresente !== null && (
+            <div
+              className={cn(
+                "badge text-white rounded-xl w-max px-3 font-semibold",
+                isPresente ? "badge-success" : "badge-error"
+              )}
+            >
+              {isPresente ? "Presente" : "Ausente"}
+            </div>
+          )}
         </div>
 
         {/* Data, Horário, Local */}
@@ -42,9 +59,27 @@ const EventCard = () => {
       {/* Confirmação */}
       <div className="flex flex-row justify-between">
         {/* Btns */}
-        <div className="flex flex-row gap-5">
-          <Button>Presente</Button>
-          <Button>Ausente</Button>
+        <div className="flex flex-row gap-2">
+          {/* Confirmar */}
+          <button
+            onClick={() => setPresente(true)}
+            className={cn(
+              "flex flex-row gap-2 px-2 py-2 border border-border rounded-md font-semibold text-sm items-center justify-center cursor-pointer",
+              isPresente ? "text-white bg-success border-success" : ""
+            )}
+          >
+            <Check height={20} /> Confirmar Presença
+          </button>
+          {/* Ausencia */}
+          <button
+            onClick={() => setPresente(false)}
+            className={cn(
+              "flex flex-row gap-2 px-2 py-2 border border-border text-error rounded-md font-semibold text-sm items-center justify-center cursor-pointer",
+              isPresente === false ? "text-white bg-error border-error" : ""
+            )}
+          >
+            <X height={20} /> Justificar Ausência
+          </button>
         </div>
         {/* Nominata */}
         <p>Ver Nominata</p>
