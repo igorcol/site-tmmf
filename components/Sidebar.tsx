3e416a3978b7cd6aef5a000f91be94a81a9db2a9
igorcol <igor.colombini@gmail.com>
2025-03-29 01:22:08 -0300

    feat: navbar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,15 +1,70 @@
-import React from 'react'
+"use client";
+import { navigationLinks } from "@/constants";
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
 
 const Sidebar = () => {
-  return (
-    <div className=''>
-        <div>
-            <div className='mt-10 flex-col gap-5'>
-                
-            </div>
-        </div>
-    </div>
-  )
-}
+  const pathname = usePathname();
 
-export default Sidebar
+  return (
+    <div className="w-[250px] sticky left-0 top-0 flex h-screen flex-col justify-between px-5 pb-5 pt-10">
+      <div>
+        <div className="mt-10 flex-col gap-5">
+          {navigationLinks.map((link) => {
+            const isSelected =
+              (pathname.includes(link.route) && link.route.length > 1) ||
+              pathname === link.route;
+
+            return (
+              <Link
+                href={link.route}
+                key={link.route}
+               >
+                <div
+                  className={cn("flex flex-row items-center w-full gap-x-5 rounded-lg px-5 py-3.5 max-md:justify-center", isSelected && "bg-tmmf shadow-sm")}
+                >
+                  <div className="relative size-5">
+                    <Image
+                      src={link.img}
+                      alt="icon"
+                      fill
+                      className={`${
+                        isSelected ? "brightness-0 invert" : "brightness-0"
+                      } object-contain`}
+                    />
+                  </div>
+                  <p
+                    className={cn(
+                      "text-base font-medium max-md:hidden",
+                      isSelected ? "text-white" : "text-dark"
+                    )}
+                  >
+                    {link.title}
+                  </p>
+                </div>
+              </Link>
+            );
+          })}
+        </div>
+      </div>
+
+      <div className="cursor-pointer my-8 flex w-full flex-row gap-2 rounded-full border border-gray-300 px-6 py-2 shadow-sm max-md:px-2">
+        {/* <Avatar>
+          <AvatarFallback className="bg-amber-100">
+            {getInitials(session?.user?.name || "U")}
+          </AvatarFallback>
+        </Avatar> */}
+
+        <div className="flex flex-col max-md:hidden">
+          <p className="font-semibold text-gray-950">User Name</p>
+          <p className="text-gray-400 text-xs">user@gmail.com</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
---
+++ constants/index.ts
@@ -0,0 +1,23 @@
+
+export const navigationLinks = [
+    {
+        title: 'Início',
+        route: '/',
+        img: '/icons/home.svg',
+    },
+    {
+        title: 'Nominata',
+        route: '/nominata',
+        img: '/icons/receipt.svg',
+    },
+    {
+        title: 'Perfil',
+        route: '/profile',
+        img: '/icons/user.svg',
+    },
+    {
+        title: 'Configurações',
+        route: '/settings',
+        img: '/icons/id.svg',
+    },
+]
