41aa805a25a5efca2f3146ff76b5809d46d7a191
igorcol <igor.colombini@gmail.com>
2025-03-29 01:27:24 -0300

    feat: Responsive Sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -10,7 +10,7 @@ const Sidebar = () => {
   const pathname = usePathname();
 
   return (
-    <div className="w-[250px] sticky left-0 top-0 flex h-screen flex-col justify-between px-5 pb-5 pt-10">
+    <div className="w-[250px] max-md:w-[100px] sticky left-0 top-0 flex h-screen flex-col justify-between px-5 pb-5 pt-5 border-r border-border">
       <div>
         <div className="mt-10 flex-col gap-5">
           {navigationLinks.map((link) => {
